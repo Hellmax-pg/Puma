b271225900ad7da3037ab0c8b8ad5840ddd43480
Maks <pegashev95@gmail.com>
2022-05-06 04:18:29 +0300

    add section testimonials

---
+++ src/components/bem-blocks/testimonials-slider/_testimonials-slider.js
@@ -0,0 +1,7 @@
+const testimonialsSwiper = new Swiper('.swiper.swiper__testimonials-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next.swiper__testimonials-slider__next',
+      prevEl: '.swiper-button-prev.swiper__testimonials-slider__prev',
+    }
+  });
