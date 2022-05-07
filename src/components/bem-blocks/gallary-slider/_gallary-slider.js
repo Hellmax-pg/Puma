7176d435b2801cd8913e4a80e4c7b9b2982fdebd
Maks <pegashev95@gmail.com>
2022-05-07 03:42:26 +0300

    add tablet + mobile adaptive

--- src/components/bem-blocks/gallary-slider/_gallary-slider.js
+++ src/components/bem-blocks/gallary-slider/_gallary-slider.js
@@ -1,13 +1,59 @@
-const gallarySwiper = new Swiper('.swiper.swiper__gallary-slider', {
-    loop: true,
-    slidesPerView: 4,
+const mediaQueryMobile = window.matchMedia('(min-width:1px) and (max-width: 766px)');
+const mediaQueryTablet = window.matchMedia('(min-width:766px) and (max-width: 1280px)');
+const mediaQueryDesktop = window.matchMedia('(min-width:1280px) and (max-width: 2540px)');
 
+function handleTabletChangeMobile(e) {
+  if (e.matches) {
+    const gallarySwiper = new Swiper('.swiper.swiper__gallary-slider', {
+      loop: true,
+      slidesPerView: 1,
   
-    navigation: {
-      nextEl: '.swiper-button-next.swiper__gallary-slider__next',
-      prevEl: '.swiper-button-prev.swiper__gallary-slider__prev',
-    }
-  });
+    
+      navigation: {
+        nextEl: '.swiper-button-next.swiper__gallary-slider__next',
+        prevEl: '.swiper-button-prev.swiper__gallary-slider__prev',
+      }
+    });
+  }
+};
+
+
+function handleTabletChangeTablet(e) {
+  if (e.matches) {
+    const gallarySwiper = new Swiper('.swiper.swiper__gallary-slider', {
+      loop: true,
+      slidesPerView: 2,
+  
+    
+      navigation: {
+        nextEl: '.swiper-button-next.swiper__gallary-slider__next',
+        prevEl: '.swiper-button-prev.swiper__gallary-slider__prev',
+      }
+    });
+  }
+};
+
+function handleTabletChangeDesktop(e) {
+  if (e.matches) {
+    const gallarySwiper = new Swiper('.swiper.swiper__gallary-slider', {
+      loop: true,
+      slidesPerView: 4,
+  
+    
+      navigation: {
+        nextEl: '.swiper-button-next.swiper__gallary-slider__next',
+        prevEl: '.swiper-button-prev.swiper__gallary-slider__prev',
+      }
+    });
+  }
+};
+
+mediaQueryTablet.addListener(handleTabletChangeTablet)
+handleTabletChangeTablet(mediaQueryTablet)
+mediaQueryDesktop.addListener(handleTabletChangeDesktop)
+handleTabletChangeDesktop(mediaQueryDesktop)
+mediaQueryMobile.addListener(handleTabletChangeMobile)
+handleTabletChangeMobile(mediaQueryMobile)
 
 const linkList = document.querySelectorAll('.gallary__categories-link')
 
